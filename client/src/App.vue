5a8d71587a66492e810c720af5c556984994f332
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-11-14 16:00:51 +0100

    handle transliteration

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,63 +2,133 @@
   <header class="sticky row color">
     <div class="col-sm-12 col-md-10 col-md-offset-1 color">
       <!-- <a href="/" class="logo color">Toolset</a> -->
-      <button :class="{ 'active': state === 'latbe' }" @click="state = 'latbe'">BEL ⇒ LAT </button>
-      <button :class="{ 'active': state === 'latru' }" @click="state = 'latru'">RUS ⇒ LAT</button>
-      <button :class="{ 'active': state === 'enbe' }" @click="state = 'enbe'">BEL ⇒ ENG</button>
-      <button :class="{ 'active': state === 'char' }" @click="state = 'char'">Decoder</button>
-      <a href="https://philology.by/" target="_blank" class="button">Philology.BY</a>
+      <button v-for="(val, key) in tools" :class="{ active: state === key }" @click="switchState(key)" :key="key">
+        {{ val[0] }}
+      </button>
     </div>
   </header>
-
-
   <div class="row">
-
     <div class="col-sm-12 col-md-10 col-md-offset-1">
-      <br />
-      <h3>Scientific transliteration (romanization)</h3>
+      <h3>{{ tools[state][1] }}</h3>
       <hr />
-      {{ state }}
-      <p><a href="/latbe">for Belarusian texts</a></p>
-      <p><a href="/latru">for Russian texts</a></p>
-
-      <!-- <a href="/be.html" class="button">Belarusian</a> -->
-      <!-- <a href="/ru.html" class="button">Russian</a> -->
+      <textarea style="width: 100%; background-color: #fdfdfd40" class="uni" v-model="txt"></textarea>
+      <div>
+        <button type="button" class="cl" @click="test">Test</button>
+        <button type="button" class="cl" @click="clear">Clear</button>
+        <button type="button" class="char" :disabled="!txt" @click="run">{{ tools[state][2] }}</button>
+      </div>
+      <!-- <div class=" light " style="margin-top:20px;">
+      </div> -->
+      <div class="section light result" style="margin-top: 20px" v-if="result" v-html="result"></div>
     </div>
   </div>
-  <footer class="row color" style="width:100%">
+  <footer class="row color" style="width: 100%">
     <div class="col-sm-12 col-md-10 col-md-offset-1">
-      <!-- Транслітарацыя беларускіх і рускіх тэкстаў для патрэб навуковых публікацый (славістыка, тэорыя мовы) -->
-      Linguistic Toolset for character level processing.<br />Транслітарацыя беларускіх тэкстаў для патрэб навуковых
-      публікацый (славістыка, тэорыя мовы)
+      <a href="https://en.wikipedia.org/wiki/Scientific_transliteration_of_Cyrillic" target="_blank">Wikipedia on
+        Romanization</a>
+      <div>
+        Linguistic Toolset for character level processing.
+      </div>
+      <div>
+        Транслітарацыя беларускіх тэкстаў для навуковых
+        публікацый.
+      </div>
     </div>
-
-    <div class="col-sm-12 col-md-10 col-md-offset-1 ">
-      &copy; <a href="https://yaskevich.com/" target="_blank" style="text-decoration:none;">Alyaxey Yaskevich</a>,
-      2018, 2023 @ Philology.BY
+    <div class="col-sm-12 col-md-10 col-md-offset-1">
+      &copy; <a href="https://yaskevich.com/" target="_blank" style="text-decoration: none">Alyaxey Yaskevich</a>, 2018,
+      2023 @ <a href="https://philology.by/" target="_blank">Philology.BY</a>
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView, useRoute } from 'vue-router'
-import { ref } from 'vue'
-const vuerouter = useRoute();
+// import { RouterLink, RouterView, useRoute } from 'vue-router'
+import { ref } from 'vue';
+// const vuerouter = useRoute();
+import tables from '../../data/tables.json';
+import unicode from '../../data/unicode-data.json';
+
+const hash = Object.fromEntries(unicode.map(item => [item.codeval, item]));
+const maps = tables as { [key: string]: { [key: string]: string } };
 const path = window.location.pathname?.slice(1);
+console.log('path', path);
+const state = ref(path || 'bellat');
+const txt = ref('');
+const result = ref('');
+const randomEmoji = String.fromCodePoint(Number('12851' + String(Math.floor(Math.random() * 9) + 1)));
 
-console.log("kek", vuerouter.params, path);
+const tools = {
+  bellat: [
+    'BEL ⇒ LAT',
+    'Scientific transliteration (romanization) for Belarusian texts',
+    'Transliterate',
+    'Здароў, марозны, звонкі вечар! Здароў, скрыпучы, мяккі снег!',
+  ],
+  ruslat: [
+    'RUS ⇒ LAT',
+    'Scientific transliteration (romanization) for Russian texts',
+    'Transliterate',
+    'Мороз и солнце; день чудесный!',
+  ],
+  beleng: ['BEL ⇒ ENG', 'Practical transcription of Belarusian texts for English speakers', 'Transcript', 'А пад мостам рыба з хвостам'],
+  char: [
+    (window.screen.width > 450 ? 'Decoder ' : '') + randomEmoji,
+    'Character codes', 'Reveal characters!', '🐒 любіць coyc з coi, です'],
+} as { [key: string]: Array<string> };
 
-const state = ref('latbe');
+const clear = () => {
+  txt.value = result.value = '';
+};
 
-const handle = (str: string) => {
-  console.log("clicked", str);
+const switchState = (str: string | number) => {
+  console.log('switch', str);
+  state.value = String(str);
+  history.pushState({}, '', "/" + str);
+  clear();
+};
 
-}
+const run = () => {
+  console.log('run', state.value);
+  if (state.value === 'char') {
+    // ${x?.codePointAt(0)}
+    result.value = Array.from(txt.value)
+      .map(x => `<div class="row"><div class="col-sm-1"><div class="box-colored">${x}</div></div><div class="col-sm-11"><div class="box-colored">${x ? hash?.[x?.codePointAt(0)?.toString(16)?.toUpperCase()?.padStart(4, '0')!]?.charname : '<UNKNOWN>'}</div></div></div>`)
+      .join('');
+    return;
+  }
 
+  const mapping = maps[state.value];
+  const content = txt.value;
+  let res = '';
+  for (let i = 0; i < content.length; i++) {
+    let ch = content.charAt(i);
+    if (ch === '\n') {
+      res += '<br/>';
+      continue;
+    }
+    res += mapping?.[ch] || ch;
+  }
+
+  if (state.value === 'beleng') {
+    res = res.replace(/(?<=e|o|a|\s|-|ߴ)ⁱ(?=e|a)/g, 'y');
+    res = res.replace(/ (?=y\s)/g, '');
+  }
+  result.value = res;
+};
+
+const test = () => {
+  txt.value = tools[state.value][3];
+  run();
+};
 </script>
 <style scoped>
 .active,
 .color {
   background: #283593;
   color: #fafafa !important;
+}
+
+.result {
+  padding: 1rem;
 }
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,12 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 import 'mini.css/dist/mini-nord.min.css';
 
 const app = createApp(App)
 
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
