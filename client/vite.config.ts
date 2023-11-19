333088356bae17cc1f6a89d3824dbf46d6fd9e1e
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-11-19 12:29:50 +0100

    update Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  plugins: [vue()],
+  server: {
+    port: Number(loadEnv('', process.cwd(), 'VITE_PORT')?.['VITE_PORT']) || 8080,
+  },
+});
